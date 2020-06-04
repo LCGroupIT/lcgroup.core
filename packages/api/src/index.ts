559b37c05b5174ba0ffba28b92c318dc9b9253d4
Sergey Romanchuk <rezety@gmail.com>
2020-06-04 12:32:48 +0700

    reorganize only auth

--- packages/api/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './simply-api.tokens';
-export * from './simply-api.module';
-export * from './simply-api.service';
-export * from './simply-api.options';
-export * from './blob-error-http.interceptor';
--- packages/api/src/simply-api.tokens.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { ISerializer } from './simply-api.options';
-
-export const API_ENDPOINT: InjectionToken<string> = new InjectionToken<string>('API_ENDPOINT');
-export const API_SERIALIZER: InjectionToken<ISerializer> = new InjectionToken<ISerializer>('API_SERIALIZER');
-
